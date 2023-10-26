3380381b60f02d2aa2c51e14d338da0fe2d6d53b
Filatov <o.filatov@rendez-vous.ru>
2023-10-26 13:59:59 +0300

    Added README

---
+++ HW_4_OTUS_2023/HW_4_OTUS_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  HW_4_OTUS_2023App.swift
+//  HW_4_OTUS_2023
+//
+//  Created by Филатов Олег Олегович on 26.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct HW_4_OTUS_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
